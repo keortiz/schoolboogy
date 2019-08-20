282be2df84b0104fa2bd2d09ed93e255065ccb41
keortiz <54082764+keortiz@users.noreply.github.com>
2019-08-20 14:39:04 -0400

    Add files via upload

---
+++ myclasses/index.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html  lang="en">
+<head>
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" type="text/css" href="styles/login.css">
+	
+
+<script src="js/jquery.js"></script>
+<script src="jquery-ui/jquery-ui.js"></script>
+
+
+		<script type='text/javascript' src='unitegallery/js/unitegallery.min.js'></script> 
+		
+		<link rel='stylesheet' href='unitegallery/css/unite-gallery.css' type='text/css' /> 
+		<script type='text/javascript' src='unitegallery/themes/default/ug-theme-default.js'></script> 
+		<link rel='stylesheet' href='unitegallery/themes/default/ug-theme-default.css' type='text/css' /> 
+
+
+
+<script src="js/bootstrap.js"></script>
+<script src="js/script.js"></script>	
+
+ <script src="https://cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js"></script>
+  <script src="https://cdn.datatables.net/1.10.12/js/dataTables.bootstrap.min.js"></script>  
+  <link rel="stylesheet" href="https://cdn.datatables.net/1.10.12/css/dataTables.bootstrap.min.css" />
+     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+ 
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+
+	
+<title>SchoolBoogy myClasses</title>
+
+</head>
+
+
+
+<body id="body">
+	<?php
+		include 'html/login.php';
+
+	?>
+	
+
+
+</body>
+</html>
+
